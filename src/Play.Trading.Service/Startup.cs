883547903f5b5640d37c0194a192f5c70d1b7e65
Gio <t-giovannyh@Gios-MacBook-Pro.local>
2024-10-01 14:31:47 -0500

    Added generic health check

--- src/Play.Trading.Service/Startup.cs
+++ src/Play.Trading.Service/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Play.Common.HealthChecks;
 using Play.Common.Identity;
 using Play.Common.MassTransit;
 using Play.Common.MongoDB;
@@ -63,6 +64,11 @@ namespace Play.Trading.Service
             services.AddSingleton<IUserIdProvider, UserIdProvider>()
                     .AddSingleton<MessageHub>()
                     .AddSignalR();
+            
+            // Health Checks
+            services
+                .AddHealthChecks()
+                .AddMongoDB(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -96,6 +102,9 @@ namespace Play.Trading.Service
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<MessageHub>("/messagehub"); // SignalR Hub communication endpoint
+                
+                // Map endpoints to Healthchecks
+                endpoints.MapPlayEconomyHealthChecks();
             });
         }
 
